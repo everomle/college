f8996eef732dcefb6a008b3e598e071f9ee7249a
unknown <everomle@gmail.com>
2013-08-04 17:29:23 -0600

    Haciendo una prueba con git

--- proyectoFinal/public_html/script.js
+++ proyectoFinal/public_html/script.js
@@ -1,6 +1,5 @@
 /* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Esta clase representa una prueba
  */
 
 function Prueba (pIdPrueba, pNombre, pDescripcion, pFechaCreacion, pPuntosTotales){
